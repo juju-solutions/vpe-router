7244a07274328354b73113cbcecdd28ada6bc08b
Adam Israel <adam@adamisrael.com>
2016-02-05 13:46:25 +0100

    Bubble up stderr via `subprocess.CalledProcessError` so it can be returned to the user via `action_fail()`

--- reactive/vpe_router.py
+++ reactive/vpe_router.py
@@ -4,6 +4,7 @@ from charmhelpers.core.hookenv import (
     status_set,
     action_get,
     action_fail,
+    log,
 )
 
 from charms.reactive import (
@@ -16,7 +17,7 @@ from charms.reactive import (
 )
 
 from charms import router
-
+import subprocess
 
 cfg = config()
 
@@ -28,18 +29,22 @@ def validate_config():
         If the ssh credentials are available, we'll act as a proxy charm.
         Otherwise, we execute against the unit we're deployed on to.
         """
-        # import pdb; pdb.set_trace()
-
         if all(k in cfg for k in ['pass', 'vpe-router', 'user']):
-            out, err = router.ssh(['whoami'], cfg.get('vpe-router'),
-                                  cfg.get('user'), cfg.get('pass'))
-            if out.strip() != cfg.get('user'):
-                raise Exception('invalid credentials')
+            routerip = cfg['vpe-router']
+            user = cfg['user']
+            passwd = cfg['pass']
+
+            if routerip and user and passwd:
+                out, err = router.ssh(['whoami'], routerip,
+                                      user, passwd)
+                if out.strip() != user:
+                    raise Exception('invalid credentials')
 
         set_state('vpe.configured')
         status_set('active', 'ready!')
 
     except Exception as e:
+        log(repr(e))
         remove_state('vpe.configured')
         status_set('blocked', 'validation failed: %s' % e)
 
@@ -65,7 +70,6 @@ def add_corporation():
     '''
     Create and Activate the network corporation
     '''
-
     domain_name = action_get('domain-name')
     iface_name = action_get('iface-name')
     # HACK: python's list, used deeper, throws an exception on ints in a tuple
@@ -77,63 +81,73 @@ def add_corporation():
     status_set('maintenance', 'adding corporation {}'.format(domain_name))
 
     """
-    $ ip link add link eth3 name eth3.103 type vlan id 103
+    Attempt to run all commands to add the network corporation. If any step
+    fails, abort and call `delete_corporation()` to undo.
     """
-    router.ip('link',
-              'add',
-              'link',
-              iface_name,
-              'name',
-              iface_vlanid,
-              'type',
-              'vlan',
-              'id',
-              vlan_id)
+    try:
+        """
+        $ ip link add link eth3 name eth3.103 type vlan id 103
+        """
+        router.ip('link',
+                  'add',
+                  'link',
+                  iface_name,
+                  'name',
+                  iface_vlanid,
+                  'type',
+                  'vlan',
+                  'id',
+                  vlan_id)
 
+        """
+        $ ip netns add domain
+        """
+        router.ip('netns',
+                  'add',
+                  domain_name)
 
-    """
-    $ ip netns add domain
-    """
-    router.ip('netns',
-              'add',
-              domain_name)
+        """
+        $ ip link set dev eth3.103 netns corpB
+        """
+        router.ip('link',
+                  'set',
+                  'dev',
+                  iface_vlanid,
+                  'netns',
+                  domain_name)
 
-    """
-    $ ip link set dev eth3.103 netns corpB
-    """
-    router.ip('link',
-              'set',
-              'dev',
-              iface_vlanid,
-              'netns',
-              domain_name)
+        """
+        $ ip netns exec corpB ip link set dev eth3.103 up
+        """
+        router.ip('netns',
+                  'exec',
+                  domain_name,
+                  'ip',
+                  'link',
+                  'set',
+                  'dev',
+                  iface_vlanid,
+                  'up')
 
-    """
-    $ ip netns exec corpB ip link set dev eth3.103 up
-    """
-    router.ip('netns',
-              'exec',
-              domain_name,
-              'ip',
-              'link',
-              'set',
-              'dev',
-              iface_vlanid,
-              'up')
+        """
+        $ ip netns exec corpB ip address add 10.0.1.1/24 dev eth3.103
+        """
+        router.ip('netns',
+                  'exec',
+                  domain_name,
+                  'ip',
+                  'address',
+                  'add',
+                  cidr,
+                  'dev',
+                  iface_vlanid)
 
-    """
-    $ ip netns exec corpB ip address add 10.0.1.1/24 dev eth3.103
-    """
-    router.ip('netns',
-              'exec',
-              domain_name,
-              'ip',
-              'address',
-              'add',
-              cidr,
-              'dev',
-              iface_vlanid)
-    status_set('active', 'ready!')
+    except subprocess.CalledProcessError as e:
+        delete_corporation()
+        action_fail('Command failed: %s (%s)' % (' '.join(e.cmd), str(e.output)))
+    finally:
+        remove_state('vpe.add-corporation')
+        status_set('active', 'ready!')
 
 
 @when('vpe.configured')
@@ -144,117 +158,124 @@ def delete_corporation():
 
     status_set('maintenance', 'deleting corporation {}'.format(domain_name))
 
-    """
-    Remove all tunnels defined for this domain
-
-    $ ip netns exec domain_name ip tun show
-        | grep gre
-        | grep -v "remote any"
-        | cut -d":" -f1`
-    """
-    p = router.ip(
-        'netns',
-        'exec',
-        domain_name,
-        'ip',
-        'tun',
-        'show',
-        '|',
-        'grep',
-        'gre',
-        '|',
-        'grep',
-        '-v',
-        '"remote any"',
-        '|',
-        'cut -d":" -f1'
-    )
-
-    # `p` should be a tuple of (stdout, stderr)
-    tunnels = p[0].split('\n')
-
-    for tunnel in tunnels:
+    try:
         """
-        $ ip netns exec domain_name ip link set $tunnel_name down
+        Remove all tunnels defined for this domain
+
+        $ ip netns exec domain_name ip tun show
+            | grep gre
+            | grep -v "remote any"
+            | cut -d":" -f1`
         """
-        router.ip(
+        p = router.ip(
             'netns',
             'exec',
             domain_name,
             'ip',
-            'link',
-            'set',
-            tunnel,
-            'down'
+            'tun',
+            'show',
+            '|',
+            'grep',
+            'gre',
+            '|',
+            'grep',
+            '-v',
+            '"remote any"',
+            '|',
+            'cut -d":" -f1'
         )
 
+        # `p` should be a tuple of (stdout, stderr)
+        tunnels = p[0].split('\n')
+
+        for tunnel in tunnels:
+            """
+            $ ip netns exec domain_name ip link set $tunnel_name down
+            """
+            router.ip(
+                'netns',
+                'exec',
+                domain_name,
+                'ip',
+                'link',
+                'set',
+                tunnel,
+                'down'
+            )
+
+            """
+            $ ip netns exec domain_name ip tunnel del $tunnel_name
+            """
+            router.ip(
+                'netns',
+                'exec',
+                domain_name,
+                'ip',
+                'tunnel',
+                'del',
+                tunnel
+            )
+
         """
-        $ ip netns exec domain_name ip tunnel del $tunnel_name
+        Remove all interfaces associated to the domain
+
+        $ ip netns exec domain_name ifconfig | grep mtu | cut -d":" -f1
         """
-        router.ip(
+        p = router.ip(
             'netns',
             'exec',
             domain_name,
-            'ip',
-            'tunnel',
+            'ifconfig',
+            '|',
+            'grep mtu',
+            '|',
+            'cut -d":" -f1'
+        )
+
+        ifaces = p[0].split('\n')
+        for iface in ifaces:
+
+            """
+            $ ip netns exec domain_name ip link set $iface down
+            """
+            router.ip(
+                'netns',
+                'exec',
+                domain_name,
+                'ip',
+                'link',
+                'set',
+                iface,
+                'down'
+            )
+
+            """
+            $ ip link del dev $iface
+            """
+            router.ip(
+                'link',
+                'del',
+                'dev',
+                iface
+            )
+
+        """
+        Remove the domain
+
+        $ ip netns del domain_name
+        """
+        router.ip(
+            'netns',
             'del',
-            tunnel
+            domain_name
         )
-
-    """
-    Remove all interfaces associated to the domain
-
-    $ ip netns exec domain_name ifconfig | grep mtu | cut -d":" -f1
-    """
-    p = router.ip(
-        'netns',
-        'exec',
-        domain_name,
-        'ifconfig',
-        '|',
-        'grep mtu',
-        '|',
-        'cut -d":" -f1'
-    )
-
-    ifaces = p[0].split('\n')
-    for iface in ifaces:
-
-        """
-        $ ip netns exec domain_name ip link set $iface down
-        """
-        router.ip(
-            'netns',
-            'exec',
-            domain_name,
-            'ip',
-            'link',
-            'set',
-            iface,
-            'down'
-        )
-
-        """
-        $ ip link del dev $iface
-        """
-        router.ip(
-            'link',
-            'del',
-            'dev',
-            iface
-        )
-
-    """
-    Remove the domain
-
-    $ ip netns del domain_name
-    """
-    router.ip(
-        'netns',
-        'del',
-        domain_name
-    )
-    status_set('active', 'ready!')
+    except:
+        # Do nothing
+        log('delete-corporation failed.')
+        pass
+    finally:
+        remove_state('vpe.delete-corporation')
+        status_set('active', 'ready!')
 
 
 @when('vpe.configured')
@@ -279,72 +300,78 @@ def connect_domains():
 
     status_set('maintenance', 'connecting domains')
 
-    """
-    $ ip tunnel add tunnel_name mode gre local local_ip remote remote_ip dev
-        iface_name key tunnel_key csum
-    """
-    router.ip(
-        'tunnel',
-        'add',
-        config['tunnel-name'],
-        'mode',
-        config['tunnel-type'],
-        'local',
-        config['local-ip'],
-        'remote',
-        config['remote-ip'],
-        'dev',
-        config['iface-name'],
-        'key',
-        config['tunnel-key'],
-        'csum'
-    )
+    try:
+        """
+        $ ip tunnel add tunnel_name mode gre local local_ip remote remote_ip dev
+            iface_name key tunnel_key csum
+        """
+        router.ip(
+            'tunnel',
+            'add',
+            config['tunnel-name'],
+            'mode',
+            config['tunnel-type'],
+            'local',
+            config['local-ip'],
+            'remote',
+            config['remote-ip'],
+            'dev',
+            config['iface-name'],
+            'key',
+            config['tunnel-key'],
+            'csum'
+        )
 
-    """
-    $ ip link set dev tunnel_name netns domain_name
-    """
-    router.ip(
-        'link',
-        'set',
-        'dev',
-        config['tunnel-name'],
-        'netns',
-        config['domain-name']
-    )
+        """
+        $ ip link set dev tunnel_name netns domain_name
+        """
+        router.ip(
+            'link',
+            'set',
+            'dev',
+            config['tunnel-name'],
+            'netns',
+            config['domain-name']
+        )
 
-    """
-    $ ip netns exec domain_name ip link set dev tunnel_name up
-    """
-    router.ip(
-        'netns',
-        'exec',
-        config['domain-name'],
-        'ip',
-        'link',
-        'set',
-        'dev',
-        config['tunnel-name'],
-        'up'
-    )
+        """
+        $ ip netns exec domain_name ip link set dev tunnel_name up
+        """
+        router.ip(
+            'netns',
+            'exec',
+            config['domain-name'],
+            'ip',
+            'link',
+            'set',
+            'dev',
+            config['tunnel-name'],
+            'up'
+        )
 
-    """
-    $ ip netns exec domain_name ip address add internal_local_ip peer
-        internal_remote_ip dev tunnel_name
-    """
-    router.ip(
-        'netns',
-        'exec',
-        config['domain-name'],
-        'ip',
-        'address',
-        'add',
-        config['internal-local-ip'],
-        'peer',
-        config['internal-remote-ip'],
-        'dev',
-        config['tunnel-name']
-    )
-    status_set('active', 'ready!')
+        """
+        $ ip netns exec domain_name ip address add internal_local_ip peer
+            internal_remote_ip dev tunnel_name
+        """
+        router.ip(
+            'netns',
+            'exec',
+            config['domain-name'],
+            'ip',
+            'address',
+            'add',
+            config['internal-local-ip'],
+            'peer',
+            config['internal-remote-ip'],
+            'dev',
+            config['tunnel-name']
+        )
+    except subprocess.CalledProcessError as e:
+        delete_domain_connection()
+        action_fail('Command failed: %s (%s)' % (' '.join(e.cmd), str(e.output)))
+    finally:
+        remove_state('vpe.connect-domains')
+        status_set('active', 'ready!')
 
 
 @when('vpe.configured')
@@ -356,27 +383,31 @@ def delete_domain_connection():
 
     status_set('maintenance', 'deleting domain connection: {}'.format(domain))
 
-    """
-    $ ip netns exec domain_name ip link set tunnel_name down
-    """
-    router.ip('netns',
-              'exec',
-              domain,
-              'ip',
-              'link',
-              'set',
-              tunnel_name,
-              'down')
+    try:
+        """
+        $ ip netns exec domain_name ip link set tunnel_name down
+        """
+        router.ip('netns',
+                  'exec',
+                  domain,
+                  'ip',
+                  'link',
+                  'set',
+                  tunnel_name,
+                  'down')
 
-    """
-    $ ip netns exec domain_name ip tunnel del tunnel_name
-    """
-    router.ip('netns',
-              'exec',
-              domain,
-              'ip',
-              'tunnel',
-              'del',
-              tunnel_name)
-
-    status_set('active', 'ready!')
+        """
+        $ ip netns exec domain_name ip tunnel del tunnel_name
+        """
+        router.ip('netns',
+                  'exec',
+                  domain,
+                  'ip',
+                  'tunnel',
+                  'del',
+                  tunnel_name)
+    except:
+        pass
+    finally:
+        remove_state('vpe.delete-domain-connection')
+        status_set('active', 'ready!')
